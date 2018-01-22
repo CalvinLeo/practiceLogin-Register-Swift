cd01c6e76c58424cca6b8730a5a99aec43c8acc5
CalvinLeo <349562074@qq.com>
2018-01-22 09:29:33 +0800

    新建类

---
+++ swift-login&register/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-login&register
+//
+//  Created by admin on 2018/1/22.
+//  Copyright © 2018年 Calvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("测试")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
